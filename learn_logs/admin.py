903575ec37813fa829ff44fb46778ba51f379a60
tammi <tammiii@163.com>
2024-03-26 14:41:07 +0800

    some data

--- learn_logs/admin.py
+++ learn_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from learn_logs.models import Topic
+from learn_logs.models import Topic, Entry
 
 # Register your models here.
 admin.site.register(Topic)
+admin.site.register(Entry)
