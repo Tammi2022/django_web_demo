514254ff942d527336718de9684e4ec146b9916d
tammi <tammiii@163.com>
2024-03-26 15:57:01 +0800

    topics list

--- learn_logs/urls.py
+++ learn_logs/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
-from .views import IndexLearnLogsView
+from .views import IndexLearnLogsView,ListLearnLogsView
 
 urlpatterns = [
     # path('bc', csrf_exempt(ExportBCOrderView.as_view())),
     path('index', csrf_exempt(IndexLearnLogsView.as_view())),
+    path('topics', csrf_exempt(ListLearnLogsView.as_view())),
 ]
--- learn_logs/views.py
+++ learn_logs/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
+from learn_logs.models import Topic
 from utils.auth import auth
 from utils.obj_response import ObjectResp
 
@@ -25,3 +26,11 @@ class IndexLearnLogsView(View):
 #     """学习笔记的主页"""
 #     # return render(request, 'learning_logs/index.html')
 #     return render(request, 'learn_logs_index.html')
+
+class ListLearnLogsView(View):
+    def get(self, request):
+        # 显示所有的主题
+        # topics = Topic.objects.all()
+        topics = Topic.objects.order_by('date_added')
+        context = {'topics': topics}
+        return render(request, 'learn_logs_topics.html', context)
