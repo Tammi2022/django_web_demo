f3713e66bc2ab5f155ddf61a3a4f9ca7971337ab
tammi <tammiii@163.com>
2024-03-28 11:32:36 +0800

    error in login

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from users.views import UserLoginView, UserLogoutView
+from users.views import UserLoginView, UserLogoutView,UserRegisterView
 
 urlpatterns = [
     # path('bc', csrf_exempt(ExportBCOrderView.as_view())),
     path('login', csrf_exempt(UserLoginView.as_view()), name='users_login'),
     path('logout', csrf_exempt(UserLogoutView.as_view()), name='users_logout'),
+    path('register', csrf_exempt(UserRegisterView.as_view()), name='users_register'),
     ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -8,13 +9,46 @@ from django.views import View
 # Create your views here.
 class UserLoginView(View):
     def get(self, request):
-        return render(request, 'users_login.html')
+        form = AuthenticationForm(request.POST)
+        context = {'form': form}
+        return render(request, 'users_login.html', context)
 
     def post(self, request):
-        return render(request, 'users_login.html')
+        form = AuthenticationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            username = form.username
+            password = form.password
+            print(username, password)
+            try:
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return HttpResponseRedirect(reverse('learn_logs_index'))  # 重定向到登录成功后的页面
+            except Exception as e:
+                print(e)
+                return HttpResponseRedirect(reverse('learn_logs_index'))  # 重定向到登录成功后的页面
+        return HttpResponseRedirect(reverse('learn_logs_index'))  # 重定向到登录成功后的页面
 
 
 class UserLogoutView(View):
     def get(self, request):
-        logout(request) # 注销用户
+        logout(request)  # 注销用户
         return HttpResponseRedirect(reverse('users_login'))
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        # 显示空的注册表单
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'users_register.html', context)
+
+    def post(self, request):
+        # 处理填写好的表单
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # 让用户自动登录，再重定向到主页
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+        return HttpResponseRedirect(reverse('learn_logs_index'))
